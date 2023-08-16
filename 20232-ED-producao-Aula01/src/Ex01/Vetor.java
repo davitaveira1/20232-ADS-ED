d409b795176d9c62fe9c166588dbfaa6fb14f676
Davi Taveira <davitaveira@gmail.com>
2023-08-16 19:49:31 -0300

    aula02-inicial

--- 20232-ED-producao-Aula01/src/Ex01/Vetor.java
+++ 20232-ED-producao-Aula01/src/Ex01/Vetor.java
@@ -12,6 +12,7 @@ public class Vetor {
     
     Aluno[] alunos = new Aluno[100];
     
+    //Inserção em vetor descompactado
     public void inserirFinalVetorV1(Aluno a){
         for(int i=0;i<alunos.length;i++){
             if(alunos[i]==null){
@@ -25,6 +26,16 @@ public class Vetor {
         for(int i=0;i<alunos.length;i++){
             if(alunos[i]!=null){
                 System.out.println("Pos: "+i+" Nome: "+alunos[i].nome);
+            }
+        }
+    }
+    
+    //Inserção em vetor compactado e posição igual a null
+    public void inserirFinalVetorV2(Aluno a){
+        for(int i=0;i < alunos.length; i++){
+            if(alunos[i]==null){
+                alunos[i]=a;
+                break;
             }
         }
     }
